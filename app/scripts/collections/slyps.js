8e5898e7063438b0b6fa9cd2cba8b22fc252f1cd
James Groeneveld <jgroeneveld@infleciton.com>
2015-06-24 23:02:28 +1000

    integrated falcon api with backbone app

--- app/scripts/collections/slyps.js
+++ app/scripts/collections/slyps.js
@@ -4,6 +4,23 @@ define(["marionette", "models/slyp"], function(Marionette, Slyp){
     model: Slyp,
     parse: function(resp){
       return resp.response;
+    },
+    createFromUrl: function(url){
+		$.ajax({
+	      url: "/blacksmith/v1/slyps",
+	      method: "POST",
+	      contentType: "application/json",
+	      data: JSON.stringify({slyp_url: url}),
+	      collection: this,
+	      success: function(response){
+
+	      	this.collection.trigger("addingNewSlyp"); // to bump off the current first slyp in feed
+	        this.collection.create(response, {at: 0});
+	      },
+	      error: function(status, response){
+	        console.log(status)
+	      }
+	    });
     }
   });
 
