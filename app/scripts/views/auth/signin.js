bf859a068354620c6fba2cb62d97e3fe91672cde
Xander <xander@inflection.com>
2015-07-11 12:18:33 -0700

    updated signup/in process to be via email

--- app/scripts/views/auth/signin.js
+++ app/scripts/views/auth/signin.js
@@ -8,11 +8,7 @@ define(["marionette"], function(){
     events: {
       "click .js-authenticate-submit"  : "submit",
       "submit form": "submit",
-      "click .js-sign-in": "signin",
       "click .js-facebook-connect": "fbLogin",
-      "click .js-signup-submit"  : "submitSignup",
-      "click .js-sign-up": "signup",
-      "click .js-sign-in": "signin",
     },
 
     initialize: function(){
@@ -35,45 +31,11 @@ define(["marionette"], function(){
       $("#facebook-login").appendTo("#facebook-button-on-form");
     },
 
-    signup: function(e){
-      e.preventDefault();
-      this.$el.find("#signin-page").hide();
-      this.$el.find("#signup-page").show();
-    },
-
-    signin: function(e){
-      e.preventDefault();
-      this.$el.find("#signin-page").show();
-      this.$el.find("#signup-page").hide();
-    },
-
     submit: function(event) {
       event.preventDefault();
       var self = this;
       var data = {
-        email: $("#signin-page [name=email]", this.el).val(),
-        password: $("#signin-page [name=password]", this.el).val()
-      };
-
-      $.ajax({
-        type: "POST",
-        url: window.apiHost + "/users/auth",
-        data: data,
-        error: function(req, resp){
-          alert("Auth failed");
-        },
-        success: function(resp){
-          App.authorized = true;
-          Backbone.history.navigate("", {trigger: true});
-        }
-      });
-    },
-    submitSignup: function(event) {
-      event.preventDefault();
-      var self = this;
-      var data = {
-        email: $("#signup-page [name=email]", this.el).val(),
-        password: $("#signup-page [name=password]", this.el).val()
+        email: $("#signin-page [name=email]", this.el).val()
       };
 
       $.ajax({
@@ -85,10 +47,11 @@ define(["marionette"], function(){
         },
         success: function(resp){
           App.authorized = true;
-          Backbone.history.navigate("", {trigger: true});
+          alert("check your email");
         }
       });
     },
+
     close: function(){
       alert("successful authentication");
     }
