5a47bcbaa42ab932f53ffad3b44baa0992b391be
Xander Groeneveld <xander.groeneveld@gmail.com>
2015-08-04 21:56:16 -0700

    added icons and tooltips

--- app/scripts/models/user.js
+++ app/scripts/models/user.js
@@ -4,6 +4,17 @@ define(["marionette"], function(){
     defaults: {
       id: null,
       email: ""
+    },
+    getIconColor: function(){
+    	var index = this.emailToInt() % 7;
+    	colors = ["red", "green-light", "green-dark", "blue-light", "blue-dark", "orange", "purple", "violet"];
+    	return colors[index];
+    },
+    emailToInt: function(){
+    	var str = this.get("email");
+    	var length = str.length;
+    	var val = str.charCodeAt(length-1) + str.charCodeAt(1) + str.charCodeAt(length/2)
+    	return val;
     }
   });
   return user;
--- app/scripts/views/chat/chatIcon.js
+++ app/scripts/views/chat/chatIcon.js
@@ -5,7 +5,15 @@ define(["marionette"], function(Marionette){
       "click .slyp-chat-user-container": "selectChat"
     },
     serializeData: function(){
-      return {email: this.model.get("users").first().get("email"), id: this.model.get("users").first().get("id"), selected: this.model.get("selected"), icon_url: this.model.get("users").first().get("icon_url")};
+      var email = this.model.get("users").first().get("email");
+      var icon_data = {
+        email: email, 
+        id: this.model.get("users").first().get("id"), 
+        selected: this.model.get("selected"), 
+        icon_color: this.model.get("users").first().getIconColor(),
+        icon_letter: email[0],
+      };
+      return icon_data;
     },
     selectChat: function(){
       this.model.setSelected();
