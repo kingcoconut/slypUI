2e9745ab7ae316c247a9f83ad0478790cc594378
Drew <dvmonroe6@gmail.com>
2015-09-07 20:15:39 -0700

    new possible grok box and search bar

---
+++ app/scripts/views/interface/filterBox.js
@@ -0,0 +1,44 @@
+define(["marionette"], function(){
+  var filterBox = Backbone.Marionette.ItemView.extend({
+    template: "#js-filter-box-tmpl",
+
+    ui: {
+      listItem: '.js-list',
+      searchInput: '.js-search',
+      clearInput: '.js-clear',
+      input: '.js-input'
+    },
+
+    events: {
+      "click @ui.listItem"  : "addActiveList",
+      "click @ui.searchInput"  : "addActiveInput",
+      "click @ui.clearInput"  : "removeInputActive",
+      "focus @ui.input"  : "addActiveParent"
+    },
+
+    addActiveList: function(event){
+      this.ui.searchInput.removeClass('input_active');
+      this.ui.listItem.removeClass('filter_active');
+      $(event.target).addClass('filter_active');
+    },
+
+    addActiveInput: function(event){
+      this.ui.listItem.removeClass('filter_active');
+      $(event.target).addClass('input_active');
+    },
+
+    removeInputActive: function(event){
+      this.ui.searchInput.removeClass('input_active');
+      debugger;
+      this.ui.listItem.first().addClass('filter_active');
+      event.stopImmediatePropagation();
+    },
+
+    addActiveParent: function(event){
+      this.ui.listItem.removeClass('filter_active');
+      $(event.target.parentElement).addClass('input_active');
+    }
+  });
+
+  return filterBox;
+});
